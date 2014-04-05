06753742dc132dad28f90f6920f2242fff5c4c6f
John's Account <John's Account@John>
2014-04-05 12:25:29 -0500

    Added command line argument for channel joining

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -7,9 +7,22 @@ public class Testing extends PircBot
 {
     public static void main(String[] args) throws IOException, IrcException
     {
+        
         MyBot bot = new MyBot();
         bot.setVerbose(true);
         bot.connect("irc.twitch.tv", 6667, "oauth:olwvp3qjls791d0hagk9smdln0eqqgy");
+        if(!args[0].equalsIgnoreCase(""))
+        {
+            if(args[0].charAt(0) != '#')
+            {
+                args[0] = "#" + args[0];
+                MyBot.CHANNEL = args[0];
+            }
+            else
+            {
+                MyBot.CHANNEL = args[0];
+            }
+        }
         bot.joinChannel(MyBot.CHANNEL);
     }
 }
